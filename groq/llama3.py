a9c38890ff22e416ea6fd921e90a05b6b2c27cec
Noshin Faria <noshin@co.design>
2025-04-13 17:04:24 +0600

    ollama3 implementation

--- groq/llama3.py
+++ groq/llama3.py
@@ -1,3 +1,6 @@
+import sys
+print("Using Python from:", sys.executable)
+
 import streamlit as st 
 import os
 from langchain_groq import ChatGroq
@@ -18,7 +21,7 @@ groq_api_key = os.getenv('GROQ_API_KEY')
 
 st.title("Chatgroq With Llama3 Demo")
 
-llm = ChatGroq(hroq_api_key = groq_api_key,
+llm = ChatGroq(groq_api_key = groq_api_key,
                model_name = "Llama3-8b-8192")
 
 prompt = ChatPromptTemplate.from_template(
@@ -34,10 +37,13 @@ Question:{input}
 )
 
 def vector_embedding():
-    st.session_state.embeddings = OllamaEmbeddings()
-    st.session_state.loader = PyPDFDirectoryLoader("./us_census")
-    st.session_state.docs = st.session_state.loader.load()
-    st.
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = OllamaEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("./huggingface/us_census") # Document Ingestion
+        st.session_state.docs = st.session_state.loader.load() # Document Loading
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200) # Chunk Creation
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) # Splitting
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings) # Vector OpenAI Embeddings
 
 
 prompt1 = st.text_input("Enter Your Question From Documents")
@@ -45,3 +51,22 @@ prompt1 = st.text_input("Enter Your Question From Documents")
 if st.button("Documents Embeddings"):
     vector_embedding()
     st.write("Vector Store DB Is Ready")
+
+
+import time
+
+if prompt1:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':prompt1})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
